860c042a7a3272965e01e146e86f984f0d468ff2
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2021-08-01 19:31:10 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
+let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
 const draggableElements = document.querySelectorAll('.draggable');
 
 draggableElements.forEach(draggableElement => {
     console.log(draggableElement);
-    
+
     const draggableHeader = draggableElement.querySelector('.draggable__header');
 
-    const onDrag = ({movementX, movementY}) => {
+    const onDrag = (e) => {
         let getStyle = window.getComputedStyle(draggableElement),
-            left = parseInt(getStyle.left),
-            top = parseInt(getStyle.top);
+        left = parseInt(getStyle.left),
+        top = parseInt(getStyle.top);
+        if (!isMobile.any()) {
+            let movementX = e.movementX,
+                movementY = e.movementY;
+            draggableElement.style.left = `${left + movementX}px`;
+            draggableElement.style.top = `${top + movementY}px`;
+        } else {
+            let pageX = e.pageX,
+            pageY = e.pageY;
+            draggableElement.style.left = `${left + pageX}px`;
+            draggableElement.style.top = `${top + pageY}px`;
+            
+        }
 
-        draggableElement.style.left = `${left + movementX}px`;
-        draggableElement.style.top = `${top + movementY}px`;
     };
 
     draggableHeader.addEventListener('mousedown', () => {
@@ -23,9 +35,11 @@ draggableElements.forEach(draggableElement => {
         draggableHeader.removeEventListener('mousemove', onDrag);
     });
 
-    draggableHeader.addEventListener('touchstart', () => {
+    draggableHeader.addEventListener('touchstart', (e) => {
         draggableHeader.classList.add('active');
         draggableHeader.addEventListener('tauchmove', onDrag);
+        console.log(e);
+
     });
     document.addEventListener('touchend', () => {
         draggableHeader.classList.remove('active');
